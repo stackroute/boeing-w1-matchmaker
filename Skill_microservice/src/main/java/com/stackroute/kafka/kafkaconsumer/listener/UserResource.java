f64c146bee46f5b086ea86e243549cd32a3a0586
RAJAT <rajsrdjnaik@gmail.com>
2018-08-09 14:24:29 +0530

    Skill microservice gets updated pom

--- Skill_microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/UserResource.java
+++ Skill_microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/UserResource.java
@@ -1,29 +1,26 @@
 package com.stackroute.kafka.kafkaconsumer.listener;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.stackroute.kafka.kafkaconsumer.model.Skill;
-
 
 @RestController
 @RequestMapping("kafka")
-public class KafkaProducer {
+public class UserResource {
 
-   @Autowired
-   private KafkaTemplate<String, Skill> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, Skill> kafkaTemplate;
 
-   private static final String TOPIC = "Kafka_Producer";
+    private static final String TOPIC = "Kafka_Example";
 
-   @GetMapping("/publish/{name}")
-   public String post(@PathVariable("name") final String name) {
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) {
 
-       kafkaTemplate.send(TOPIC, new Skill(name, "Technology", name, name, name));
+        kafkaTemplate.send(TOPIC, new Skill("","Rajat", "", "", ""));
 
-       return "Published successfully";
-   }
+        return "Published successfully";
+    }
 }
