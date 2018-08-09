1f5f2b1bcccfd134987322649435f22ea9ea0b0c
sathwik-pancheti <sathwik10.pancheti@gmail.com>
2018-08-09 16:45:36 +0530

    service renamed

--- certification-Service/src/main/java/com/stackroute/kafka/consumer/configuration/KafkaConsumerConfiguration.java
+++ certification-Service/src/main/java/com/stackroute/kafka/consumer/configuration/KafkaConsumerConfiguration.java
@@ -24,7 +24,7 @@ public class KafkaConsumerConfiguration {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.238.163:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
