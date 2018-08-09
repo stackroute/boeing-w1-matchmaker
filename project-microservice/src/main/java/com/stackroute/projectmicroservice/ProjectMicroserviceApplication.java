be70d756f4e5c3c070a8b60d121b49ff9fb08951
Ansh Shah <anshs918@gmail.com>
2018-08-09 12:40:33 +0530

    adding project

---
+++ project-microservice/src/main/java/com/stackroute/projectmicroservice/ProjectMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.projectmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMicroserviceApplication.class, args);
+	}
+}
