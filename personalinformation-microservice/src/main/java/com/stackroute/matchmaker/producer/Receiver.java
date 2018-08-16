8747df7fac2c91d3c1bc79da42035625663de545
Joe0405 <joeabrahamk@yahoo.com>
2018-08-16 14:22:38 +0530

    added image folder in Angular assets

--- personalinformation-microservice/src/main/java/com/stackroute/matchmaker/producer/Receiver.java
+++ personalinformation-microservice/src/main/java/com/stackroute/matchmaker/producer/Receiver.java
@@ -2,21 +2,21 @@ package com.stackroute.matchmaker.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.stackroute.matchmaker.model.PersonalInfo;
 
 @Service
 public class Receiver {
-	
+
 	@Autowired
 	private Producer producer;
-	
+
 	public void receiveObject(PersonalInfo personalInfo) {
-		
-		//dummy object to be produced
-		PersonalInfo per = new PersonalInfo("hello","hello","hello","hello","hello","hello","hello","hello","hello","hello","hello","hello");
+
+		// dummy object to be produced
+		PersonalInfo per = new PersonalInfo("hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello",
+				"hello", "hello", "hello", "hello");
 		producer.sendObject(per);
-		
+
 	}
 
 }
