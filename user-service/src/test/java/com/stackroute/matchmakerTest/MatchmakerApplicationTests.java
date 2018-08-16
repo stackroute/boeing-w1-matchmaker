e3272c2f7a0c73155d0e7f081e77b28434957530
Nimbuzzz99 <sharoon8105@gmail.com>
2018-08-16 11:50:27 +0530

    errors and login updated

--- user-service/src/test/java/com/stackroute/matchmakerTest/MatchmakerApplicationTests.java
+++ user-service/src/test/java/com/stackroute/matchmakerTest/MatchmakerApplicationTests.java
@@ -3,7 +3,6 @@ package com.stackroute.matchmakerTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
