635e02e62036382e9a950b47fb6a47a26b1f31f2
vikramIH <vrihanji@gmail.com>
2018-08-20 13:52:36 +0530

    Modified Indexer Message

---
+++ location-microservice/src/test/java/com/stackroute/kafka/locationconsumer/test/indexermodel/TargetNodePropertiesTest.java
@@ -0,0 +1,24 @@
+package com.stackroute.kafka.locationconsumer.test.indexermodel;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.meanbean.test.BeanTester;
+
+import com.stackroute.kafka.locationconsumer.indexermodel.TargetNodeProperties;
+
+public class TargetNodePropertiesTest {
+
+	private TargetNodeProperties targetNodeProperties;
+
+	@Before
+	public void setUp() throws Exception {
+		targetNodeProperties = new TargetNodeProperties();
+	    targetNodeProperties.setId("abc123");
+	}
+
+	@Test
+	public void Beantest() {
+		new BeanTester().testBean(TargetNodeProperties.class);
+	}
+
+}
