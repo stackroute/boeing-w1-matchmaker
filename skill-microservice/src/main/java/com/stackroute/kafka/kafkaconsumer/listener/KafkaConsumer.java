bb346fac1e80808e44acd3400cad76d7e095dfb5
RAJAT <rajsrdjnaik@gmail.com>
2018-08-20 11:33:12 +0530

    Updated Skill microservices for Indexer

--- skill-microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/KafkaConsumer.java
+++ skill-microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/KafkaConsumer.java
@@ -1,11 +1,10 @@
 package com.stackroute.kafka.kafkaconsumer.listener;
 
+// Organize Import ctrl+shift+O
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import com.stackroute.kafka.kafkaconsumer.model.Skill;
 
 @Service
 public class KafkaConsumer {
@@ -19,14 +18,16 @@ public class KafkaConsumer {
 
 	// kafka string or default listener
 
-	@KafkaListener(topics = "Location_String", groupId = "group_id")
+	@KafkaListener(topics = "Location_String", group = "group_id")
 	public void consume(String message) {
 		LOG.info("Consumed message='{}'", message);
 	}
 
-//kafka json listener, specifying the listener container factory
-	@KafkaListener(topics = "${listener.topic.name}", groupId = "group_json", containerFactory = "skillKafkaListenerFactory")
-	public void consumeJson(Skill skill) {
-		LOG.info("Consumed JSON message='{}'", skill);
-	}
+	// kafka json listener, specifying the listener container factory
+	// @KafkaListener(topics = "${listener.topic.name}", group = "group_json",
+	// containerFactory = "skillKafkaListenerFactory")
+	// public void consumeJson(Skill skill) {
+	// LOG.info("Consumed JSON message='{}'", skill);
+	// }
+
 }
--- skill-microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/KafkaProducer.java
+++ skill-microservice/src/main/java/com/stackroute/kafka/kafkaconsumer/listener/KafkaProducer.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.stackroute.kafka.kafkaconsumer.model.Skill;
 
 @RestController
@@ -15,17 +14,28 @@ import com.stackroute.kafka.kafkaconsumer.model.Skill;
 public class KafkaProducer {
 
 	// using template defined in configuration class
-	@Autowired
-	private KafkaTemplate<String, Skill> kafkaTemplate;
 
+	@Autowired
+	private KafkaTemplate<String, Skill> kafkaTemplateskill;
+
+	// private ProcessedSkill processed_skill;
 	@Value("${listener.topic.name}")
 	private String TOPIC;
 
+	// @GetMapping("/publishIndexer/{id}")
+	// public String post(@PathVariable("id") final String id) {
+	//
+	// // publishing to kafka
+	// kafkaTemplate.send(topic, new ProcessedSkill(id, "Java", "moderate"));
+	//
+	// return "Published successfully";
+	// }
+
 	@GetMapping("/publish/{name}")
-	public String post(@PathVariable("name") final String name) {
+	public String postskill(@PathVariable("name") final String name) {
 
 		// publishing to kafka
-		kafkaTemplate.send(TOPIC, new Skill(name, "Technology", name, name, name));
+		kafkaTemplateskill.send(TOPIC, new Skill(name, name, name, name, name, name));
 
 		return "Published successfully";
 	}
