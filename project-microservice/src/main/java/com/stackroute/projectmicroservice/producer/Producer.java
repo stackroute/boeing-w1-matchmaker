eab6a4c51e3a0f1700483597afd38f144085d156
Ansh Shah <anshs918@gmail.com>
2018-08-20 16:17:27 +0530

    Added Producer accordingly

--- project-microservice/src/main/java/com/stackroute/projectmicroservice/producer/Producer.java
+++ project-microservice/src/main/java/com/stackroute/projectmicroservice/producer/Producer.java
@@ -7,18 +7,27 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.stackroute.projectmicroservice.indexermodel.Indexer;
+import com.stackroute.projectmicroservice.model.Project;
 
 @Service
 public class Producer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
 
+//	@Autowired
+//	private KafkaTemplate<String, Indexer> kafkaTemplate;
+//
+//	public void send(Indexer indexer) {
+//		LOG.info("sending JSON message='{}'", indexer);
+//		kafkaTemplate.send("ProjectIndexer", indexer);
+//	}
+	
 	@Autowired
-	private KafkaTemplate<String, Indexer> kafkaTemplate;
+	private KafkaTemplate<String, Project> kafkaTemplate;
 
-	public void send(Indexer indexer) {
-		LOG.info("sending JSON message='{}'", indexer);
-		kafkaTemplate.send("ProjectIndexer", indexer);
+	public void send(Project project) {
+		LOG.info("sending JSON message='{}'", project);
+		kafkaTemplate.send("ProjectIndexer", project);
 	}
 
 }
