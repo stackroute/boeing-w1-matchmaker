af6ac9cf3208d10a64b4048946270328ea4c3cae
Joe0405 <joeabrahamk@yahoo.com>
2018-08-21 11:24:15 +0530

    pmd changes

--- personalinformation-microservice/src/main/java/com/stackroute/matchmaker/PersonalInformationApplication.java
+++ personalinformation-microservice/src/main/java/com/stackroute/matchmaker/PersonalInformationApplication.java
@@ -3,9 +3,12 @@ package com.stackroute.matchmaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*main application class*/
 @SpringBootApplication
-public class PersonalInformationApplication {
-	public static void main(String[] args) {
+public class PersonalInformationApplication { 
+	
+	/*main function*/
+	public static void main(String[] args) {  
 		SpringApplication.run(PersonalInformationApplication.class, args);
 	}
 }
--- personalinformation-microservice/src/main/java/com/stackroute/matchmaker/configuration/ProducerConfigs.java
+++ personalinformation-microservice/src/main/java/com/stackroute/matchmaker/configuration/ProducerConfigs.java
@@ -8,7 +8,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -39,7 +38,7 @@ public class ProducerConfigs {
 		//specifies the serializer class for key that implements the org.apache.kafka.common.serialization.Serializer interface.
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		//specifies the serializer class for value that implements the org.apache.kafka.common.serialization.Serializer interface.
-		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return configProps;
 	}
 	
