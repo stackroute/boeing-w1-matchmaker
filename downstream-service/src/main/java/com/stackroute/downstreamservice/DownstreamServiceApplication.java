07c3caceef92c1609d7f150ffcdde3bf582163cc
Joe0405 <joeabrahamk@yahoo.com>
2018-08-24 19:32:13 +0530

    registering downstream with eureka

--- downstream-service/src/main/java/com/stackroute/downstreamservice/DownstreamServiceApplication.java
+++ downstream-service/src/main/java/com/stackroute/downstreamservice/DownstreamServiceApplication.java
@@ -2,11 +2,13 @@ package com.stackroute.downstreamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * 
  * @author Simran and Aakash
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DownstreamServiceApplication {
 
