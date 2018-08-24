fe039c68c0e79331772f540e06a78b9652d3c7ff
Joe0405 <joeabrahamk@yahoo.com>
2018-08-24 18:50:14 +0530

    edited downstream

--- downstream-service/src/main/java/com/stackroute/downstreamservice/DownstreamServiceApplication.java
+++ downstream-service/src/main/java/com/stackroute/downstreamservice/DownstreamServiceApplication.java
@@ -2,14 +2,12 @@ package com.stackroute.downstreamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * 
  * @author Simran and Aakash
  *
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DownstreamServiceApplication {
 
 	public static void main(String[] args) {
