d4917b39c929f4e226613e3c1c890c02a0a9fd3a
Joe0405 <joeabrahamk@yahoo.com>
2018-08-27 16:46:37 +0530

    registering search with eureka

--- search-microservice/src/main/java/com/stackroute/matchmaker/searchmicroservice/SearchMicroserviceApplication.java
+++ search-microservice/src/main/java/com/stackroute/matchmaker/searchmicroservice/SearchMicroserviceApplication.java
@@ -2,12 +2,14 @@ package com.stackroute.matchmaker.searchmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 
  * @author syam
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SearchMicroserviceApplication {
 
--- search-microservice/src/test/java/com/stackroute/matchmaker/searchmicroservice/SearchMicroserviceApplicationTests.java
+++ search-microservice/src/test/java/com/stackroute/matchmaker/searchmicroservice/SearchMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stackroute.matchmaker.searchmicroservice;
+/*package com.stackroute.matchmaker.searchmicroservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class SearchMicroserviceApplicationTests {
 	}
 
 }
+*/
