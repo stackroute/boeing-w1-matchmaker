3a8470cfed43e7b72326e1d8ced7dbece164605e
Nimbuzzz99 <sharoon8105@gmail.com>
2018-08-29 17:18:12 +0530

    autocomplete added

--- autocomplete-microservice/src/main/java/com/stackroute/matchmaker/autocompletemicroservice/AutocompleteMicroserviceApplication.java
+++ autocomplete-microservice/src/main/java/com/stackroute/matchmaker/autocompletemicroservice/AutocompleteMicroserviceApplication.java
@@ -2,11 +2,13 @@ package com.stackroute.matchmaker.autocompletemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * 
  * @author syam
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AutocompleteMicroserviceApplication {
 
