7067428501ccb2233156008cd9084ccf8b8e738d
vikramIH <vrihanji@gmail.com>
2018-08-29 23:50:56 +0530

    Changed login routing

--- Angular-project/src/app/app-routing.module.ts
+++ Angular-project/src/app/app-routing.module.ts
@@ -1,42 +1,33 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule, Router } from "@angular/router";
-import { ProfileComponent } from "./profile/profile.component";
-import { RegistrationComponent } from "./registration/registration.component";
-import { LoginComponent } from "./login/login.component";
-import { AuthGuard } from "./guards/auth.guard";
-import { PortfoliosComponent } from "./portfolios/portfolios.component";
-import { VisitingCardComponent } from "./portfolios/visiting-card/visiting-card.component";
-import { SocialMediaComponent } from "./portfolios/social-media/social-media.component";
-import { HomeComponent } from "./home/home.component";
-import { SkillPortfolioComponent } from "./portfolios/skill-portfolio/skill-portfolio.component";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { HelpComponent } from "./help/help.component";
-import { ContactComponent } from "./contact/contact.component";
+import {NgModule} from '@angular/core';
+import {Routes , RouterModule, Router} from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PortfoliosComponent } from './portfolios/portfolios.component';
+import { VisitingCardComponent } from './portfolios/visiting-card/visiting-card.component';
+import { SocialMediaComponent } from './portfolios/social-media/social-media.component';
+import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "profile", component: ProfileComponent },
-  { path: "portfolios", component: PortfoliosComponent },
-  { path: "register", component: RegistrationComponent },
-  { path: "", component: LoginComponent },
-  { path: "portfolios/visiting", component: VisitingCardComponent },
-  { path: "portfolios/socialmedia", component: SocialMediaComponent },
-  { path: "portfolios/skill", component: SkillPortfolioComponent },
-  { path: "about", component: AboutUsComponent },
-  { path: "help", component: HelpComponent },
-  { path: "contact", component: ContactComponent }
+  {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'portfolios', component: PortfoliosComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'portfolios/visiting', component: VisitingCardComponent},
+  {path: 'portfolios/socialmedia', component: SocialMediaComponent},
+  {path: '', component: StartComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-export const routingComponents = [
-  ProfileComponent,
-  PortfoliosComponent,
-  VisitingCardComponent,
-  SocialMediaComponent,
-  RegistrationComponent,
-  LoginComponent
-];
+    imports: [
+      RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule {}
+  export const routingComponents = [ProfileComponent, PortfoliosComponent, VisitingCardComponent, SocialMediaComponent,
+     RegistrationComponent, LoginComponent, StartComponent];
