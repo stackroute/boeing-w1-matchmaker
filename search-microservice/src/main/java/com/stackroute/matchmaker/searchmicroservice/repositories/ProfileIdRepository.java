47a5df6efc2afe363cdacda8ce6637b19fab6d28
Simran Narang <narang.simran30@gmail.com>
2018-08-30 12:06:37 +0530

    done with result set

--- search-microservice/src/main/java/com/stackroute/matchmaker/searchmicroservice/repositories/ProfileIdRepository.java
+++ search-microservice/src/main/java/com/stackroute/matchmaker/searchmicroservice/repositories/ProfileIdRepository.java
@@ -11,23 +11,23 @@ import com.stackroute.matchmaker.searchmicroservice.model.Result;
 
 public interface ProfileIdRepository extends Neo4jRepository<ProfileId, String> {
 
-	@Query("MATCH (p:ProfileId)-[:hasSkill]->(s:Skill) where  s.name IN {list} return p"
-			+ " UNION MATCH (p:ProfileId)-[:undergone]->()-[:coversSkill]->(s)where  s.name IN {list} RETURN p"
-			+ " UNION MATCH (p:ProfileId)-[:workedIn]->()-[:usesSkill]->(s) where s.name IN {list} RETURN p"
-			+ " UNION MATCH (s:Skill)<-[:partWholeOf]-()<-[:hasSkill]-(p) WHERE s.name IN {list} return p")
+	@Query("MATCH (p:ProfileId)-[:hasSkill]->(s:Skill) where s.name IN {list} RETURN p "
+			+ " UNION MATCH (p:ProfileId)-[:undergone]->()-[:coversSkill]->(s)where  s.name IN {list} RETURN p "
+			+ " UNION MATCH (p:ProfileId)-[:workedIn]->()-[:usesSkill]->(s) where s.name IN {list} RETURN p "
+			+ " UNION MATCH (s:Skill)<-[:partWholeOf]-()<-[:hasSkill]-(p) WHERE s.name IN {list} return p ")
 	public List<ProfileId> getBySkill(@Param("list") List<String> list);
 
-	@Query("MATCH (p:ProfileId)-[r:livedIn]->(c:City) WHERE c.city IN {list} RETURN p"
-			+ "UNION MATCH (p:ProfileId)-[s:livesIn]->(c:city) WHERE c.city IN {list} RETURN p")
+	@Query("MATCH (p:ProfileId)-[r:livedIn]->(c:City) WHERE c.city IN {list} RETURN p "
+			+ "UNION MATCH (p:ProfileId)-[s:livesIn]->(c:city) WHERE c.city IN {list} RETURN p ")
 	public List<ProfileId> getByLocation(@Param("list") List<String> list);
 
-	@Query("MATCH (p:ProfileId)-[r:employeeOf]->(c:Company) WHERE c.name IN {list} RETURN p")
+	@Query("MATCH (p:ProfileId)-[r:employeeOf]->(c:Company) WHERE c.name IN {list} RETURN p ")
 	public List<ProfileId> getByOrganisation(@Param("list") List<String> list);
 
-	@Query("MATCH (p:ProfileId)-[r:employeeOf]->(c:Company) WHERE sum(toInteger(r.duration)) >= {year} RETURN p")
-	public List<ProfileId> getByExperince(int year);
-	
-	@Query("MATCH (p:ProfileId)-[:hasSkill]->(s:Skill),(p)-[r:employeeOf]->(),(p)-[:livesIn]->(c:City) WHERE p.profileId IN {profileId} RETURN p.profileId,s.name,sum(toInteger(r.duration)),c.city")
-	public List<Result> getResultSet(List<ProfileId> profileId);
+	@Query("MATCH (p:ProfileId)-[r:employeeOf]->(c:Company) WITH p,SUM(r.duration) AS sum  WHERE sum >= {year} RETURN p ")
+	public List<ProfileId> getByExperince(@Param("year") int year);
+
+	@Query("MATCH (p:ProfileId)-[:hasSkill]->(s:Skill),(p)-[r:employeeOf]->(),(p)-[:livesIn]->(c:City) WHERE p.profileId = {profileId} RETURN p.profileId as profileId,collect(s.name) as skills,r.duration as experince,c.city as city")
+	public Result getResultSet(@Param("profileId") String profileId);
 
 }
