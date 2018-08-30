ad0ee47609352f22f1060482f6a0b08c2f2c0328
vikramIH <vrihanji@gmail.com>
2018-08-30 11:17:09 +0530

    profile updated

--- Angular-project/src/app/socket-connection/socket-connection.component.ts
+++ Angular-project/src/app/socket-connection/socket-connection.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from "jquery";
 import * as socket from '../../assets/socket.js' ;
-var jquery: any;
 
+var jquery: any;
 @Component({
   selector: 'app-socket-connection',
   templateUrl: './socket-connection.component.html',
   styleUrls: ['./socket-connection.component.css']
 })
 export class SocketConnectionComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
--- Angular-project/src/assets/socket.js
+++ Angular-project/src/assets/socket.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS("http://172.23.238.207:8080/gs-guide-websocket");
+    var socket = new SockJS("http://172.23.238.211:8080/gs-guide-websocket");
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
