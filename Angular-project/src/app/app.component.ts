cfc7d0f8bbd322b86feae65a585f2e29da8c0aff
vikramIH <vrihanji@gmail.com>
2018-08-31 15:16:00 +0530

    sjnjnj

--- Angular-project/src/app/app.component.ts
+++ Angular-project/src/app/app.component.ts
@@ -5,32 +5,32 @@ import { Router } from '@angular/router';
 // import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService]
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css'],
+ providers: [UserService]
 })
 export class AppComponent implements OnInit {
-  private user_check = false;
-  profile: String;
-  constructor(private authenticationService: AuthenticationService, private router: Router) {
-    authenticationService.getLoggedInName.subscribe(() => {
-      console.log('hi');
-      this.ngOnInit();
-    });
-  }
-  ngOnInit() {
-  if (localStorage.getItem('currentUser') != null) {
-    this.user_check = true;
-    this.profile = JSON.parse(localStorage.getItem('currentUser'));
-  }
-  }
+ private user_check = false;
+ profile: String;
+ constructor(private authenticationService: AuthenticationService, private router: Router) {
+   authenticationService.getLoggedInName.subscribe(() => {
+     console.log('hi');
+     this.ngOnInit();
+   });
+ }
+ ngOnInit() {
+ if (localStorage.getItem('currentUser') != null) {
+   this.user_check = true;
+   this.profile = JSON.parse(localStorage.getItem('currentUser'));
+ }
+ }
 
-  logout() {
-    this.authenticationService.logout();
-    this.router.navigate(['/login']);
-    this.user_check = false;
-  }
+ logout() {
+   this.authenticationService.logout();
+   this.router.navigate(['/login']);
+   this.user_check = false;
+ }
 
 
 }
