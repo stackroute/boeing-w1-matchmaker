41e02bbf4093008fbbe8db69433b2c6dc67786db
vikramIH <vrihanji@gmail.com>
2018-08-31 15:21:45 +0530

    sjnjnj

--- Angular-project/src/app/portfolios/skill-portfolio/skill-portfolio.component.ts
+++ Angular-project/src/app/portfolios/skill-portfolio/skill-portfolio.component.ts
@@ -18,15 +18,15 @@ export class SkillPortfolioComponent implements OnInit {
   ngOnInit() {
   }
 
-  // getVisitingCard() {
-  //   this.get().subscribe( data => {
-  //      this.UserData = data;
+  getVisitingCard() {
+    this.get().subscribe( data => {
+       this.UserData = data;
 
-  //   });
-  // }
+    });
+  }
 
-  // get(): Observable<any> {
-  //   return this.http.get(`http://13.232.19.29:8092/downstream/api/v1/user/${JSON.parse(localStorage.getItem('currentUser'))}`);
-  // }
+  get(): Observable<any> {
+    return this.http.get(`http://13.232.19.29:8092/downstream/api/v1/user/${JSON.parse(localStorage.getItem('currentUser'))}`);
+  }
 
 }
