d6ea616c2db31dcb4a565baf0b76749fb6fec9c1
vikramIH <vrihanji@gmail.com>
2018-08-31 09:49:04 +0530

    router changed

--- Angular-project/src/app/home/home.component.ts
+++ Angular-project/src/app/home/home.component.ts
@@ -34,7 +34,7 @@ export class HomeComponent implements OnInit {
 
   search(searchQuery: string) {
     console.log(searchQuery);
-    console.log('hello world uiu');
+    console.log('hello world yyyy');
     return this.http.get('http://http://13.232.19.29:8092/search/api/v1/search/' + searchQuery);
   }
 
